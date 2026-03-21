69d5df8ab87517ddce5e5596b77d6ddebce4e49a
Gotoushunki <shunki.goto@gmail.com>
2026-03-21 10:35:00 +0000

    Auto publish site payload 2026-03-21 19:35 JST

--- public/preview/config.js
+++ public/preview/config.js
@@ -11,7 +11,7 @@ window.SITE_CONFIG = {
   },
   "publicationStatus": {
     "displayRaceDate": "20260322",
-    "checkedAtUtc": "2026-03-21T09:47:59Z",
+    "checkedAtUtc": "2026-03-21T10:19:59Z",
     "pendingRaceDate": "",
     "headline": "3/22分を公開中です",
     "message": "最新取得日は 3/22 です。"
