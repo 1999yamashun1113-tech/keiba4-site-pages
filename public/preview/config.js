6290c468560592c3208c1988eb36a113f28a0678
Gotoushunki <shunki.goto@gmail.com>
2026-03-21 14:51:01 +0000

    Auto publish site payload 2026-03-21 23:51 JST

--- public/preview/config.js
+++ public/preview/config.js
@@ -3,26 +3,7 @@ window.SITE_CONFIG = {
   "dataBase": "data",
   "premiumEnabled": false,
   "showSourcePath": false,
-  "marketingProof": {
-    "headline": "高回収率のモデルを使用",
-    "description": "複勝の購入にそのまま使った場合は回収率が100%を超えないため、普段の予想の参考情報としてご活用ください。",
-    "validationLabel": "検証期間 2024-2025",
-    "footnote": "※学習期間: 2008-2023年 / テスト期間: 2024-2025年",
-    "strategies": [
-      {
-        "betType": "sanrenpuku",
-        "label": "三連複",
-        "roi": 1.1700900900900901,
-        "ticketCount": 555
-      },
-      {
-        "betType": "wide",
-        "label": "ワイド",
-        "roi": 1.065562403697997,
-        "ticketCount": 1298
-      }
-    ]
-  },
+  "marketingProof": {},
   "maintenanceStatus": {
     "enabled": true,
     "headline": "現在サイトメンテナンス中です",
@@ -30,7 +11,7 @@ window.SITE_CONFIG = {
   },
   "publicationStatus": {
     "displayRaceDate": "20260322",
-    "checkedAtUtc": "2026-03-21T08:55:49Z",
+    "checkedAtUtc": "2026-03-21T14:34:59Z",
     "pendingRaceDate": "",
     "headline": "3/22分を公開中です",
     "message": "最新取得日は 3/22 です。"
